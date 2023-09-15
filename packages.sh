9634dee28e4b8031ed6f5d149d4beb27285302dd
link-op <40344950+14link@users.noreply.github.com>
2023-09-15 08:35:27 +0800

    +

--- packages.sh
+++ packages.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+rm -Rf *
+git rm --cache *
+git_clone https://github.com/sirpdboy/luci-app-netdata.git
+#git_clone https://github.com/sirpdboy/luci-app-autotimeset
+#git_clone https://github.com/esirplayground/luci-app-poweroff
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
+
+git_clone https://github.com/kenzok8/small-package/luci-app-quickstart
+
+
+
+
+
+
+
 
 rm -rf .svn
 rm -rf ./*/.git
@@ -8,3 +24,6 @@ rm -rf .github
 rm LICENSE
 mv LICENSE.packages LICENSE
 exit 0
+
+
+
