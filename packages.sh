1e01c9677dc1ae7bcdafe7c2273658c0b43dd4e2
link-op <40344950+14link@users.noreply.github.com>
2023-09-15 10:12:57 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -12,12 +12,9 @@ git clone https://github.com/sirpdboy/luci-app-netdata
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 
 #git clone https://github.com/animegasan/luci-app-quickstart
-git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-quickstart  ./ ; rm -rf openwrt-packages
+#git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-quickstart  ./ ; rm -rf openwrt-packages
+git clone --depth 1 https://github.com/kenzok8/small-package && mv -n small-package/luci-app-quickstart  ./ ; rm -rf small-package
 #git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
-
-
-
-
 
 rm -rf .svn
 rm -rf ./*/.git
