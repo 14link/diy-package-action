304ff6211ebfbdd0ad9be7ce153af00ec723d8a1
link-op <40344950+14link@users.noreply.github.com>
2023-09-15 14:03:08 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 rm -Rf *
 git rm --cache *
-
+#git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
 #rm -rf luci-app-openclash
 git clone https://github.com/sirpdboy/luci-app-netdata
 #git clone https://github.com/Jason6111/luci-app-netdata
 
-#git clone https://github.com/sirpdboy/luci-app-autotimeset
-#git clone https://github.com/esirplayground/luci-app-poweroff
+git clone https://github.com/sirpdboy/luci-app-autotimeset
+git clone https://github.com/esirplayground/luci-app-poweroff
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 
+git clone --depth 1 https://github.com/kenzok8/small-package && mv -n small-package/luci-app-quickstart  small-package/quickstart ./ ; rm -rf small-package
 #git clone https://github.com/animegasan/luci-app-quickstart
 #git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-quickstart  ./ ; rm -rf openwrt-packages
-git clone --depth 1 https://github.com/kenzok8/small-package && mv -n small-package/luci-app-quickstart  small-package/quickstart ./ ; rm -rf small-package
-
-#git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
 
 rm -rf .svn
 rm -rf ./*/.git
