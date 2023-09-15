366036fc2653039c8a9250d989a68976ff89913d
link-op <40344950+14link@users.noreply.github.com>
2023-09-15 09:59:25 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -4,13 +4,16 @@ git rm --cache *
 
 #rm -rf luci-app-openclash
 git clone https://github.com/sirpdboy/luci-app-netdata
+#git clone https://github.com/Jason6111/luci-app-netdata
+
 #git clone https://github.com/sirpdboy/luci-app-autotimeset
 #git clone https://github.com/esirplayground/luci-app-poweroff
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
-git clone https://github.com/animegasan/luci-app-quickstart
 
-
+#git clone https://github.com/animegasan/luci-app-quickstart
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-quickstart  ./ ; rm -rf openwrt-packages
+#git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
 
 
 
