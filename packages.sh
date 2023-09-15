9e411fd6898bb686e3921d66e03c744ff283f4f5
link-op <40344950+14link@users.noreply.github.com>
2023-09-15 20:58:16 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -10,8 +10,10 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset
 git clone https://github.com/esirplayground/luci-app-poweroff
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
+#git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 
 git clone --depth 1 https://github.com/kenzok8/small-package && mv -n small-package/luci-app-quickstart  small-package/quickstart ./ ; rm -rf small-package
+
 #git clone https://github.com/animegasan/luci-app-quickstart
 #git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-quickstart  ./ ; rm -rf openwrt-packages
 
