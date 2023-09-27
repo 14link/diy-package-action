9e1388a46656837a665d8ad03ff7b3763e5ec976
link-op <40344950+14link@users.noreply.github.com>
2023-09-27 08:10:12 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -15,12 +15,19 @@ git clone https://github.com/esirplayground/luci-app-poweroff
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 #git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 
+#下面是抽取他人仓库中的某个插件,相当于切片处理
+#=============================================================================================
+#方法一:用svn co命令&&用trunk替换路径中的tree/main
 #svn co https://github.com/281677160/openwrt-package/trunk/luci-app-clash package/luci-app-clash
 #https://github.com/kenzok8/small-package/tree/main/luci-app-quickstart
 #https://github.com/kenzok8/small-package/tree/main/quickstart
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-quickstart
 svn co https://github.com/kenzok8/small-package/trunk/quickstart
+
+#方法二:有点绕路,但殊途同归
 #git clone --depth 1 https://github.com/kenzok8/small-package && mv -n small-package/luci-app-quickstart  small-package/quickstart ./ ; rm -rf small-package
+
+#=============================================================================================
 
 #git clone https://github.com/animegasan/luci-app-quickstart
 #git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-quickstart  ./ ; rm -rf openwrt-packages
