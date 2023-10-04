ab2b3da6bc7d3309c40b5a8d9781c6637ed2b251
link-op <40344950+14link@users.noreply.github.com>
2023-10-04 21:09:27 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -6,7 +6,9 @@
 
 git clone https://github.com/sirpdboy/luci-app-autotimeset
 git clone https://github.com/esirplayground/luci-app-poweroff
-git clone https://github.com/linkease/nas-packages-luci.git
+
+#git clone https://github.com/linkease/nas-packages.git
+#git clone https://github.com/linkease/nas-packages-luci.git
 
 #git clone https://github.com/sirpdboy/autosamba
 #git clone https://github.com/sirpdboy/luci-app-netdata
