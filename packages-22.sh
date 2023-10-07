c052095ae98a99f56603b2dafd73d13f375055fb
link-op <40344950+14link@users.noreply.github.com>
2023-10-07 15:17:48 +0800

    更新 packages-22.sh

--- packages-22.sh
+++ packages-22.sh
@@ -6,16 +6,30 @@
 
 git clone https://github.com/sirpdboy/luci-app-autotimeset
 git clone https://github.com/esirplayground/luci-app-poweroff
+#svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-autotimeset
+#DNS
+git clone https://github.com/QiuSimons/openwrt-mos.git
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
+#svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome
+#git clone https://github.com/pymumu/openwrt-smartdns.git
+#chmod 755 luci-app-adguardhome/root/etc/init.d/AdGuardHome
 
-#git clone https://github.com/QiuSimons/openwrt-mos.git
-#it clone https://github.com/linkease/nas-packages-luci.git
+#sed -i 's/默认用户名密码都是root/默认用户名密码都是admin/g' adguardhome/patches/default_username.patch 
+#sed -i 's/\<control\>/system/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
+#sed -i 's/\<Control\>/System/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
 
-#git clone https://github.com/sirpdboy/autosamba
-#git clone https://github.com/sirpdboy/luci-app-netdata
-#git clone https://github.com/Jason6111/luci-app-netdata
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
-#git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+#Theme
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
+
+git clone https://github.com/gngpp/luci-theme-design.git theme/luci-theme-design
+git clone https://github.com/gngpp/luci-app-design-config.git theme/luci-theme-design-config
+git clone https://github.com/uparrows/luci-theme-atmaterial.git theme/luci-theme-atmaterial
+git clone https://github.com/kenzok8/luci-theme-ifit.git theme/luci-theme-ifit
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git theme/luci-theme-opentopd
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git theme/luci-theme-infinityfreedom
+git clone https://github.com/thinktip/luci-theme-neobird.git theme/luci-theme-neobird
+git clone https://github.com/sirpdboy/luci-theme-kucat.git theme/luci-theme-kucat
 
 #下面是抽取他人仓库中的某个插件,相当于切片处理
 #=============================================================================================
@@ -52,4 +66,56 @@ rm -f .gitattributes .gitignore
 rm -rf .github
 rm LICENSE
 mv LICENSE.packages LICENSE
+
 exit 0
+
+rm -rf .svn
+rm -f .gitattributes .gitignore
+rm -rf .github
+rm -rf ./*/.git
+rm -rf ./*/.svn 
+rm -rf ./*/*/.git
+rm -rf ./*/*/.svn 
+rm -rf ./*/*/.github
+
+exit 0
+
+==================================================
+rm -rf feeds/packages/net/v2ray-geodata
+
+
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+=============================================================================================
+#git clone https://github.com/linkease/istore.git
+#git clone https://github.com/linkease/istore-ui.git
+#git clone https://github.com/firkerword/luci-app-lucky.git
+#git clone https://github.com/immortalwrt/homeproxy.git
+
+#svn co https://github.com/haiibo/openwrt-packages/trunk/cpulimit
+#passwall
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+#git clone https://github.com/fw876/helloworld.git package/ssr
+
+git clone -b master https://github.com/linkease/nas-packages
+git clone -b main https://github.com/linkease/nas-packages-luci
+
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
+#svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-adguardhome
+#chmod 755 luci-app-adguardhome/root/etc/init.d/AdGuardHome
+#sed -i 's/默认用户名密码都是root/默认用户名密码都是admin/g' adguardhome/patches/default_username.patch 
+#sed -i 's/\<control\>/system/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
+#sed -i 's/\<Control\>/System/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
+#git clone https://github.com/QiuSimons/openwrt-mos.git
+#it clone https://github.com/linkease/nas-packages-luci.git
+
+#git clone https://github.com/sirpdboy/autosamba
+#git clone https://github.com/sirpdboy/luci-app-netdata
+#git clone https://github.com/Jason6111/luci-app-netdata
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
+#git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
