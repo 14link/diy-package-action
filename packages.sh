8bd73b83ed74b7b40a39c7f9155b94e244bf7c71
14link <40344950+14link@users.noreply.github.com>
2025-08-24 06:54:59 +0800

    更新 packages.sh

--- packages.sh
+++ packages.sh
@@ -9,8 +9,8 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages packages && mv -n packages/luci-app-quickstart ./;rm -rf packages
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages packages && mv -n packages/quickstart ./;rm -rf packages
 
-git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha
-git clone https://github.com/animegasan/luci-app-alpha-config
+# git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha
+# git clone https://github.com/animegasan/luci-app-alpha-config
 
 # https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-quickstart
 # git clone --depth 1 https://github.com/kenzok8/luci-theme-ifit ifit && mv -n ifit/luci-theme-ifit ./;rm -rf ifit
