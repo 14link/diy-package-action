52140c4316711495a86c85a48317bb7fdc276c9c
14link <40344950+14link@users.noreply.github.com>
2025-08-29 08:40:14 +0800

    1

--- packages.sh
+++ packages.sh
@@ -4,14 +4,14 @@
 # git clone --depth 1 https://github.com/kiddin9/openwrt-packages && mv -n openwrt-packages/luci-app-bypass openwrt-packages/luci-app-fileassistant ./ ; rm -rf openwrt-packages
 # rm -rf luci-app-openclash
 
-# git clone https://github.com/linkease/istore.git
+git clone https://github.com/linkease/istore.git
 git clone https://github.com/esirplayground/luci-app-poweroff
 git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages packages && mv -n packages/luci-app-quickstart ./;rm -rf packages
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages packages && mv -n packages/quickstart ./;rm -rf packages
 
-# git clone https://github.com/sirpdboy/luci-app-autotimeset
 # git clone --depth 1 https://github.com/sirpdboy/luci-app-lucky
+# git clone https://github.com/sirpdboy/luci-app-autotimeset
 # ==================================================
 
 rm -rf .svn
